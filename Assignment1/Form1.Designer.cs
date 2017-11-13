e7658b6e0ce35d2c1f60de4cac87c16f7bdb4de0
Steven <Steven.Brown17@hotmail.com>
2017-11-13 12:37:56 -0700

    initial commit

---
+++ Assignment1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box1 = new System.Windows.Forms.TextBox();
+            this.box2 = new System.Windows.Forms.TextBox();
+            this.box3 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // box1
+            // 
+            this.box1.Location = new System.Drawing.Point(33, 56);
+            this.box1.Multiline = true;
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(100, 57);
+            this.box1.TabIndex = 0;
+            // 
+            // box2
+            // 
+            this.box2.Location = new System.Drawing.Point(33, 141);
+            this.box2.Multiline = true;
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(100, 57);
+            this.box2.TabIndex = 1;
+            // 
+            // box3
+            // 
+            this.box3.Location = new System.Drawing.Point(33, 223);
+            this.box3.Multiline = true;
+            this.box3.Name = "box3";
+            this.box3.Size = new System.Drawing.Size(100, 57);
+            this.box3.TabIndex = 2;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(161, 56);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "Submit 1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(161, 141);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "Submit 2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(161, 223);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "Submit 3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "This button will display an \r\nExclamation message box";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 26);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "This button will display an \r\nHand message box";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(158, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 26);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "This button will display an \r\nInformation message box";
+            // 
+            // btnLabel
+            // 
+            this.btnLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLabel.Location = new System.Drawing.Point(345, 141);
+            this.btnLabel.Name = "btnLabel";
+            this.btnLabel.Size = new System.Drawing.Size(123, 44);
+            this.btnLabel.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 361);
+            this.Controls.Add(this.btnLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.box3);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox box1;
+        private System.Windows.Forms.TextBox box2;
+        private System.Windows.Forms.TextBox box3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label btnLabel;
+    }
+}
+
---
+++ Assignment1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1{
+
+    /// <summary>
+    /// Form1 class contains button actions
+    /// </summary>
+    public partial class Form1 : Form {
+        public Form1(){
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// This method contains actions to be performed when btn1 is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn1_Click(object sender, EventArgs e){
+            DialogResult MyResult;
+
+            MyResult = MessageBox.Show("You typed: " + box1.Text, "Button 1", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+
+            btnLabel.Text = "You Clicked: " + MyResult.ToString();
+
+        }//end btn1click
+
+        /// <summary>
+        /// this method contains actions to be performed when btn2 is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn2_Click(object sender, EventArgs e){
+
+            DialogResult MyResult;
+
+            MyResult = MessageBox.Show("You typed: " + box2.Text, "Button 2", MessageBoxButtons.YesNo, MessageBoxIcon.Hand);
+
+            btnLabel.Text = "You Clicked: " + MyResult.ToString();
+
+        }//end btn2click
+
+        /// <summary>
+        /// this method contains actions to be performed when btn3 is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn3_Click(object sender, EventArgs e){
+
+            DialogResult MyResult;
+
+            MyResult = MessageBox.Show("You typed: " + box3.Text, "Button 3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            btnLabel.Text = "You Clicked: " + MyResult.ToString();
+
+        }//end btn3click
+
+    }//end Form1class
+
+}//end namespace
